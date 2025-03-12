759c0ce3120f13c023c57dabb232016244aa2169
Anand Singh <111451218+Michaelanand123singh@users.noreply.github.com>
2025-03-12 15:42:15 +0530

    side bar fixed

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,6 +15,21 @@ const Header: React.FC = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  // Close menu when clicking outside
+  useEffect(() => {
+    if (isMenuOpen) {
+      const handleClickOutside = (event: MouseEvent) => {
+        const target = event.target as HTMLElement;
+        if (!target.closest('nav') && !target.closest('button')) {
+          setIsMenuOpen(false);
+        }
+      };
+      
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, [isMenuOpen]);
+
   return (
     <header
       className={`fixed top-0 left-0 w-full px-6 py-4 flex justify-between items-center z-50 transition-all duration-500 ${
@@ -31,25 +46,35 @@ const Header: React.FC = () => {
         />
       </Link>
 
-      {/* Mobile Menu Button */}
-      <button
-        className="lg:hidden text-black focus:outline-none"
-        onClick={() => setIsMenuOpen((prev) => !prev)}
-        aria-label="Toggle Menu"
-      >
-        {isMenuOpen ? <X size={28} color={'black'} /> : <Menu size={28} color={'black'} />}
-      </button>
+      {/* Mobile Menu Button - Visible only when menu is closed */}
+      {!isMenuOpen && (
+        <button
+          className="lg:hidden text-black focus:outline-none"
+          onClick={() => setIsMenuOpen(true)}
+          aria-label="Open Menu"
+        >
+          <Menu size={28} color={'black'} />
+        </button>
+      )}
 
       {/* Slide-in Mobile Menu from Right */}
       <div
         className={`fixed top-0 right-0 h-full w-3/4 sm:w-1/2 bg-red-600 transition-transform transform ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
-        } lg:translate-x-0 lg:static lg:w-auto lg:bg-transparent lg:flex`}
+        } lg:translate-x-0 lg:static lg:w-auto lg:bg-transparent lg:flex z-50`}
       >
-        <nav className="p-6">
+        {/* Close button inside the mobile menu */}
+        <button
+          className="absolute top-4 right-4 lg:hidden text-white focus:outline-none"
+          onClick={() => setIsMenuOpen(false)}
+          aria-label="Close Menu"
+        >
+          <X size={28} />
+        </button>
+        
+        <nav className="p-6 mt-12 lg:mt-0">
           <ul className="flex flex-col lg:flex-row lg:space-x-6">
-           
-          <li>
+            <li>
               <Link
                 href="/"
                 className="block py-4 px-6 text-white hover:text-gray-300 transition-colors"
